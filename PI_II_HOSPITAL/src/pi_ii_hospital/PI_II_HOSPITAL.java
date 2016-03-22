5a8f33673a2c4d74e56fa1b34dad1a3bc32826ef
R2-B2 <R2-B2@192.168.0.22>
2016-03-21 23:17:10 -0300

    Atualização 1.1

--- PI_II_HOSPITAL/src/pi_ii_hospital/PI_II_HOSPITAL.java
+++ PI_II_HOSPITAL/src/pi_ii_hospital/PI_II_HOSPITAL.java
@@ -15,8 +15,8 @@ public class PI_II_HOSPITAL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Olá Mundo :)");
-        
+        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
     }
     
 }
